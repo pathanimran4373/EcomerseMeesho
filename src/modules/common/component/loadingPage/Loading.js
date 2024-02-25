793489c06aa0f93c77c3cf52957d2fbf3bafe42e
Your Name <pathanimran4373@gmail.com>
2024-02-24 22:18:12 -0800

    add loading page

---
+++ src/modules/common/component/loadingPage/Loading.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./loading.css"; 
+
+function LoadingCircle() {
+  return (
+    <div className="loading-page">
+        <div className="loading-circle"></div>
+    </div>
+  )
+}
+
+export default LoadingCircle;
